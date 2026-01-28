69c93f6e479c7fc7f115d0cbcb257bfd83791e0b
LARVAMOLT <77361162+LARVAMOLT@users.noreply.github.com>
2026-01-28 15:42:51 -0600

    Create script.js

---
+++ quotebox/script.js
@@ -0,0 +1,9 @@
+var quote = new Array ();
+quote[0] = "Fall seven times, stand up eight.";
+quote[1] = "If you want to go fast, go alone. If you want to go far, go together.";
+quote[2] = "What you see in yourself is what you see in the world.";
+quote[3] = "Speak the truth, but leave immediately after.";
+var i = Math.floor(4*Math.random())
+//this number ^ must always be one more than the number of the last entry!!!
+document.write(quote[i]);
+//get your own Random Quote Box from: larvamolt.neocities.org/quotebox
