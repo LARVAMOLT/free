f3d93207262c09391f7df825df6818d6d7c84499
LARVAMOLT <77361162+LARVAMOLT@users.noreply.github.com>
2026-01-28 19:21:31 -0600

    Create shopkeeper.js

---
+++ neoshop/shopkeeper.js
@@ -0,0 +1,9 @@
+var quote = new Array ();
+hi[0] = "Welcome!";
+hi[1] = "We have the best deals in town!";
+hi[2] = "Thanks for stopping by";
+hi[3] = "Consider yourself greeted.";
+var i = Math.floor(4*Math.random())
+//this number ^ must always be one more than the number of the last entry!!!
+document.write(hi[i]);
+//Neopets Shopkeeper Greeting Randomizer from: larvamolt.neocities.org/neoshop
